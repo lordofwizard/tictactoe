2851768d2f5848c21cd66d83bf3b01a4dd1e477d
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-02-20 14:15:20 +0530

    working game Run class / struct with run functionality that can trim any thing thats un necessary

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,10 @@ pub mod run;
 
 
 use std::io;
-use game::game::Board;
+
 
 
 fn main(){
-    println!("");
-    let mut game = Board::new();
-    game.print_board();
-    println!();
-    game.put_o(3, 3);
-    println!();
-    game.print_board();
-    println!("filled ? {}",game.filled_check());
-    game.fill_with_x();
-    println!();
-    game.print_board();
-    println!("filled ? {}",game.filled_check());
+    let game = run::Run::game_instance();
+    game.run();
 }
--- src/run.rs
+++ src/run.rs
@@ -3,22 +3,40 @@ pub struct Run{
     board_instance : Board
 }
 impl Run{
-    fn game_instance(&self){
+    pub fn game_instance() -> Run {
         let board_instance = Board::new();
+        Run { board_instance: board_instance }
     }
-    fn run(){
+    pub fn run(&self){
         use std::io; // using it here... because i don't want to keep std functions 
                      // global scope
-        let mut input :String = String::new();
-        let mut game_state : bool = true;
         
+        let mut game_state : bool = true;
+        Run::clear_screen();
+        Run::welcome();
         while game_state == true{
+            let mut input :String = String::new();
+            println!("Command :- ");
             std::io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read the message");
+            .read_line(&mut input).unwrap();
+            //.expect("Failed to read the message");
+            println!("{}",input);
+            if input.trim() == "exit" {
+                game_state = false;
+            } 
         }
 
-        
+    }
+    fn clear_screen(){
+        print!("{esc}c", esc = 27 as char);
+    }
+    fn welcome(){
+        use colored::Colorize;
+        println!("{}","     Welcome to Tic-Tac-Toe".blue());
+        println!("\n \n");
+        println!("The rules are simple. This is a 2 player tictactoe game..\n One with a row/column of same element wins.\n For more documentation refer to \n https://github.com/lordofwizard/tictactoe.git");
+        println!("{} {}","Project Author :-".yellow(),"LordOfWizard".red());
+
     }
     
 }
